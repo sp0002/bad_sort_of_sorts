8bf70836d6b5d2220dc13228f039aab7f780fcad
sp0002 <47800471+sp0002@users.noreply.github.com>
2020-10-09 21:59:19 +0800

    Check for sorted array without using built-in sorted()

--- bogo.py
+++ bogo.py
@@ -1,11 +1,19 @@
+# Small to large.
 import random
+
+def in_order(arr):
+    in_order = True
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            in_order = False
+    return in_order
 
 array = input("Enter numbers separated with a space: ")
 print("Sorting starts.")
 array = array.split(" ")
 array = [int(i) for i in array]  # Convert all elements to integer.
 
-while array != sorted(array):
+while in_order(array):
     random.shuffle(array)
     
 print("Sorted: ")
